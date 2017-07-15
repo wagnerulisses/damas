f63a110975e99238ed8cce07cbc045f6408bdd77
unknown <eduardobona@gmail.com>
2017-07-15 14:45:38 -0300

    normalizando com PSR2 o script

--- movimentar.php
+++ movimentar.php
@@ -1,25 +1,27 @@
 <?php
+/**
+ * Script para receber valores e realizar movimentação
+ * PHP Version 5
+ */
 
-	$origem = $_GET["origem"];
-	$destino = $_GET["destino"];
+$origem = $_GET["origem"];
+$destino = $_GET["destino"];
 
-	if(! file_exists('tabuleiro.json')) {
-		echo "Erro Arquivo Inexisten";
-		exit;
-	}
+if (! file_exists('tabuleiro.json')) {
+    echo "Erro Arquivo Inexisten";
+    exit;
+}
 
-	$tabuleiro = json_decode(file_get_contents('tabuleiro.json'), true);
+$tabuleiro = json_decode(file_get_contents('tabuleiro.json'), true);
 
-	include 'validar_movimentacao.php';
+include 'validar_movimentacao.php';
 
-	if (validar_movimentacao($origem, $destino)) {
-		$tabuleiro[$destino[0]][$destino[1]] = $tabuleiro[$origem[0]][$origem[1]];
-		$tabuleiro[$origem[0]][$origem[1]] = " ";
+if (validar_movimentacao($origem, $destino)) {
+    $tabuleiro[$destino[0]][$destino[1]] = $tabuleiro[$origem[0]][$origem[1]];
+    $tabuleiro[$origem[0]][$origem[1]] = " ";
 
-		file_put_contents('tabuleiro.json', json_encode($tabuleiro));
-	}else{
-		echo "Erro Movimentacao Incorreta";
-		exit;
-	}
-
-?>
+    file_put_contents('tabuleiro.json', json_encode($tabuleiro));
+} else {
+    echo "Erro Movimentacao Incorreta";
+    exit;
+}
--- test_validar_movimentacao.php
+++ test_validar_movimentacao.php
@@ -1,28 +1,26 @@
 <?php
 
-	$tabuleiro = '[[" ","B"," ","B"," ","B"," ","B"],["B"," ","B"," ","B"," ","B"," "],[" ","B"," ","B"," ","B"," ","B"],[" "," "," "," "," "," "," "," "],[" "," "," "," "," "," "," "," "],["P"," ","P"," ","P"," ","P"," "],[" ","P"," ","P"," ","P"," ","P"],["P"," ","P"," ","P"," ","P"," "]]';
-	$tabuleiro = json_decode($tabuleiro, true);
+$tabuleiro = file_get_contents('test_tabuleiro.json');
+$tabuleiro = json_decode($tabuleiro, true);
 
-	include 'validar_movimentacao.php';
+include 'validar_movimentacao.php';
 
-	$test_posicao_valida_1 = posicao_valida('00', '01', $tabuleiro);
-	if ($test_posicao_valida_1 != false) {
-		echo "Erro no teste! 00 e 01 deveriam dar FALSE";
-	}
+$test_posicao_valida_1 = posicao_valida('00', '01', $tabuleiro);
+if ($test_posicao_valida_1 != false) {
+    echo "Erro no teste! 00 e 01 deveriam dar FALSE";
+}
 
-	$test_posicao_valida_2 = posicao_valida('01', '12', $tabuleiro);
-	if ($test_posicao_valida_2 != true) {
-		echo "Erro no teste! 01 e 12 deveriam dar TRUE";
-	}	
+$test_posicao_valida_2 = posicao_valida('01', '12', $tabuleiro);
+if ($test_posicao_valida_2 != true) {
+    echo "Erro no teste! 01 e 12 deveriam dar TRUE";
+}
 
-	$test_movimentacao_diagnonal_1 = movimentacao_diagonal_frente('00', '01', $tabuleiro);
-	if($test_movimentacao_diagnonal_1 != false){
-		echo "Erro no teste! 00 e 01 deveriam dar FALSE";
-	}
+$test_movimentacao_diagnonal_1 = movimentacao_diagonal_frente('00', '01', $tabuleiro);
+if ($test_movimentacao_diagnonal_1 != false) {
+    echo "Erro no teste! 00 e 01 deveriam dar FALSE";
+}
 
-	$test_movimentacao_diagnonal_2 = movimentacao_diagonal_frente('01', '12', $tabuleiro);
-	if($test_movimentacao_diagnonal_2 != true){
-		echo "Erro no teste! 01 e 12 deveriam dar TRUE";
-	}
-
-?>
+$test_movimentacao_diagnonal_2 = movimentacao_diagonal_frente('01', '12', $tabuleiro);
+if ($test_movimentacao_diagnonal_2 != true) {
+    echo "Erro no teste! 01 e 12 deveriam dar TRUE";
+}
